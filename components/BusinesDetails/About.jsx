612a8e773a08083d6a6cdbb3569391d1cf46e340
Murtadha9 <murtadha.hussen@gmail.com>
2024-07-04 01:36:30 +0300

    scroll

--- components/BusinesDetails/About.jsx
+++ components/BusinesDetails/About.jsx
@@ -1,15 +1,15 @@
-import { View, Text, ScrollView } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import React from "react";
 
-const About = ({business}) => {
+const About = ({ business }) => {
   return (
-    <View style={{padding:20,backgroundColor:'#fff'}}>
-      <Text style={{fontSize:20,fontWeight:'bold'}}>About</Text>
-      <ScrollView>
-      <Text style={{lineHeight:25}}>{business?.about}</Text>
+    <View style={{ padding: 10, backgroundColor: "#fff", height: "100%" }}>
+      <ScrollView >
+        <Text style={{ fontSize: 20, fontWeight: "bold" }}>About</Text>
+        <Text style={{ lineHeight: 25 }}>{business?.about}</Text>
       </ScrollView>
     </View>
-  )
-}
+  );
+};
 
-export default About
+export default About;
