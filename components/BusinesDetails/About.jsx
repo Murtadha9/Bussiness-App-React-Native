c68ccbdf236a1c2d1709264bec6dbe9cf9c96740
Murtadha9 <murtadha.hussen@gmail.com>
2024-07-09 00:41:47 +0300

    done

--- components/BusinesDetails/About.jsx
+++ components/BusinesDetails/About.jsx
@@ -6,7 +6,7 @@ const About = ({ business }) => {
     <View style={{ padding: 10, backgroundColor: "#fff" }}>
 
         <Text style={{ fontSize: 20, fontWeight: "bold" }}>About</Text>
-        <Text style={{ lineHeight: 25 }}>{business?.about}</Text>
+        <Text style={{ lineHeight: 26 }}>{business?.about}</Text>
 
     </View>
   );
